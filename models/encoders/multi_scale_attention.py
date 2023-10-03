0c28ca013524faddf85003c4d01aa04cdd6fe6d0
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2023-10-02 20:20:22 -0400

    removing global fusion layer

--- models/encoders/multi_scale_attention.py
+++ models/encoders/multi_scale_attention.py
@@ -41,7 +41,7 @@ class MultiScaleAttention(nn.Module):
             self.attn_fusion.append(StokenAttentionLayer(dim, n_iter=1, 
                             sp_stoken_size=sp_stoken_size, lp_stoken_size=lp_stoken_size))
         self.attn_fusion = nn.ModuleList(self.attn_fusion)
-        self.global_fusion = iAFF(dim)
+        # self.global_fusion = iAFF(dim)
         self.final_proj = nn.Linear(dim, dim)
         self.apply(self._init_weights)
 
@@ -110,27 +110,9 @@ class MultiScaleAttention(nn.Module):
         B, N, C = A[0].shape
         output_small_patched_attn = A[0].permute(0, 2, 1).contiguous().view(B, C, H, W)
         output_small_patched_attn = self.attn_fusion[0](output_small_patched_attn, output_small_patched_attn)
-        # global_attn = A[0].permute(0, 2, 1).contiguous().view(B, C, H, W)
-        ##print('shapes: ', output.shape, global_attn.shape)
         for i in range(1,len(A)):
-            # idx = i - 1
             output_small_patched_attn = self.attn_fusion[i](output_small_patched_attn, A[i].permute(0, 2, 1).contiguous().view(B, C, H, W))
-        # output = self.global_fusion(output_small_patched_attn, global_attn)
-        # print('final fused: ',output.shape)
         return output_small_patched_attn
-
-    # def fuse_ms_attn_map(self, A, H, W):
-    #     B, N, C = A[0].shape
-    #     output_small_patched_attn = A[1].permute(0, 2, 1).contiguous().view(B, C, H, W)
-    #     # output_small_patched_attn = self.attn_fusion[0](output_small_patched_attn, output_small_patched_attn)
-    #     global_attn = A[0].permute(0, 2, 1).contiguous().view(B, C, H, W)
-    #     ##print('shapes: ', output.shape, global_attn.shape)
-    #     for i in range(2,len(A)):
-    #         idx = i - 2
-    #         output_small_patched_attn = self.attn_fusion[idx](output_small_patched_attn, A[i].permute(0, 2, 1).contiguous().view(B, C, H, W))
-    #     output = self.global_fusion(output_small_patched_attn, global_attn)
-    #     # print('final fused: ',output.shape)
-    #     return output_small_patched_attn
 
     def forward(self, x, H, W):
         ####print('!!!!!!!!!!!!attention head: ',self.num_heads, ' !!!!!!!!!!')
@@ -138,64 +120,22 @@ class MultiScaleAttention(nn.Module):
         B, N, C = x.shape
         q = self.q(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
 
-        ### 0. 71
-
-        ##3 previous 0.74... That's why we made necessary changes to go back
-        # This reduces dimension of k and v
-        # 120, 160 --Flatten--> 19200--FNN--> 300
-        # if self.sr_ratio > 1:       
-        #     x_ = x.permute(0, 2, 1).reshape(B, C, H, W) 
-        #     x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1) 
-        #     x_ = self.norm(x_)
-        #     kv = self.kv(x_).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4) 
-        # else:
-        #     kv = self.kv(x).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4) 
-        # k_full, v_full = kv[0], kv[1]
-        # ###print(f'global q:{q.shape} k:{k_full.shape} v:{v_full.shape}')
-        # a_1 = self.attention(q, k_full, v_full)
-        # ###print(f'full scale attn:{a_1.shape}')
-        # a_1 = a_1.transpose(1, 2)
-        # a_1 = a_1.reshape(B, N, C)
-        # a_1 = self.proj(a_1)
-        # a_1 = self.proj_drop(a_1)
-
-        # A.append(a_1)
-
         kv = self.kv(x).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
         k, v = kv[0], kv[1]
-        # print(f'new k:{k.shape} new v:{v.shape} q:{q.shape}')
-        # print('############################################')
+        
         for rg_shp in self.local_region_shape:
             q_patch = self.patchify(q, H, W, rg_shp)
             k_patch = self.patchify(k, H, W, rg_shp)
             v_patch = self.patchify(v, H, W, rg_shp)
-            # print('local region shape: ',rg_shp)
-            # print(f'patchified q:{q_patch.shape}, k:{k_patch.shape}, v:{v_patch.shape}')
             patched_attn = self.attention(q_patch, k_patch, v_patch)
-            # print('patched attention output: ',patched_attn.shape)
-            # exit()
             patched_attn = patched_attn.permute(0, 1, 5, 2, 3, 4).contiguous()
-            #patched_attn = patched_attn.view(0, 1, 5, 2, 3, 4)
-            #print('patched attention permute: ',patched_attn.shape)
             a_1 = patched_attn.view(patched_attn.shape[0], -1, patched_attn.shape[3], patched_attn.shape[4], rg_shp, rg_shp)
-            #print('patched attention reshape: ',a_1.shape)
             a_1 = a_1.permute(0, 1, 2, 4, 3, 5).contiguous().reshape(B, C, N)
-            #a_1 = a_1.reshape(B, C, N)
-            #exit()
-            #a_1 = patched_attn.view(patched_attn.shape[0], patched_attn.shape[1], -1, patched_attn.shape[4])
-            ###print('local attn: ',a_1.shape)
             a_1 = a_1.transpose(1, 2)
-            #a_1 = a_1.reshape(B, N, C)
-            ###print('local attn reshape: ',a_1.shape)
             a_1 = self.proj(a_1)
             a_1 = self.proj_drop(a_1)
-            ##print('local attn final: ',a_1.shape)
-            #exit()
             A.append(a_1)
 
-        #print('$$$$multi attention shapes$$$$')
-        # for attn_o in A:
-        #     #print(attn_o.shape)
         attn_fused = self.fuse_ms_attn_map(A, H, W)
         attn_fused = attn_fused.reshape(B, C, N).transpose(1, 2)
         
